2b5f2ee79e0377ff4306ee00ed4aaf1931c887e6
SODA Noriyuki <n-soda@users.sourceforge.net>
2021-10-27 01:53:15 +0900

    retain original copyright notice by Daniel Tillett for Damm.java

--- src/main/java/oss/tsukuba/utils/Damm.java
+++ src/main/java/oss/tsukuba/utils/Damm.java
@@ -1,3 +1,66 @@
+/**
+ * Copyright (c) 2021 Osamu Tatebe.  All Rights Reserved.
+ * 
+ * The authors hereby grant permission to use, copy, modify, and
+ * distribute this software and its documentation for any purpose,
+ * provided that existing copyright notices are retained in all copies
+ * and that this notice is included verbatim in any distributions.  The
+ * name of the author may not be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, ITS
+ * DOCUMENTATION, OR ANY DERIVATIVES THEREOF, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * https://github.com/DanielTillett/Damm32/
+ * Base32 implementation of the Damm error detection algorithm in C
+ * 
+ * Copyright (c) 2015, Daniel Tillett
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * Background
+ * The Damm algorithm is a check digit algorithm that detects all single-digit errors
+ * and all adjacent transposition errors.
+ * 
+ * It detects all occurrences of altering one single digit and all occurrences of
+ * transposing two adjacent digits, the two most frequent transcription errors.
+ * The Damm algorithm has the benefit that it makes do without the dedicatedly
+ * constructed permutations and its position specific powers being inherent in the
+ * Verhoeff scheme. Prepending leading base encoding zeros does not affect the check digit.
+ * https://en.wikipedia.org/wiki/Damm_algorithm
+ */
+
 package oss.tsukuba.utils;
 
 import java.security.NoSuchAlgorithmException;
