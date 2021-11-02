2f36207728f4274ccbd318c8b85ce027e29e5bbc
atsushi0804 <kuma@canaly.co.jp>
2021-11-02 10:41:28 +0900

    add columns for error information

--- src/main/java/org/oss_tsukuba/dao/Error.java
+++ src/main/java/org/oss_tsukuba/dao/Error.java
@@ -21,6 +21,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Error {
 
+	public static final int CHECK_DIGIT_ERROR = 0;
+	
+	public static final int DECRYPT_ERROR = 1;
+	
+	public static final int LENGTH_ERROR = 2;
+	
+	public static final int CHARACTER_ERROR = 3;
+	
 	@Id
 	@Column(name = "id")
 	private int id;
@@ -28,6 +36,12 @@ public class Error {
 	@Column(name = "user")
 	private String user;
 
+	@Column(name = "ip_addr")
+	private String ipAddr;
+	
+	@Column(name = "hostname")
+	private String hostname;
+	
 	@Column(name = "date")
 	private Date date;
 
@@ -36,16 +50,30 @@ public class Error {
 
 	static private SimpleDateFormat df = new SimpleDateFormat("yyyy MM/dd HH:mm:ss");
 
+	
+	public Error(String user, String ipAddr, String hostname, int type) {
+		super();
+		this.user = user;
+		this.ipAddr = ipAddr;
+		this.hostname = hostname;
+		this.date = new Date();
+		this.type = type;
+	}
+
 	public String getDispDate() {
 		return df.format(date);
 	}
 
 	public String getError() {
 		switch (type) {
-		case 0:
+		case CHECK_DIGIT_ERROR:
 			return "check digit エラー";
-		case 1:
+		case DECRYPT_ERROR:
 			return "復号化エラー";
+		case LENGTH_ERROR:
+			return "文字数エラー";
+		case CHARACTER_ERROR:
+			return "文字エラー";
 		}
 
 		return "その他";
