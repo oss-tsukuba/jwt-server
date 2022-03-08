318a8b46e4e79eb794895de191799e909f5ea7f1
atsushi0804 <kuma@canaly.co.jp>
2022-03-08 16:29:06 +0900

    add build war settings

--- src/main/java/org/oss_tsukuba/JwtServerApplication.java
+++ src/main/java/org/oss_tsukuba/JwtServerApplication.java
@@ -2,12 +2,18 @@ package org.oss_tsukuba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JwtServerApplication {
+public class JwtServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtServerApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JwtServerApplication.class);
+	}
 }
